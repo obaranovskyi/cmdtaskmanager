d93549e67cb653e9683404b80b0bd6d7cd8a66cc
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-01 07:22:23 +0200

    Minor refactoring

--- cmdtaskmanager/shared/json_file.py
+++ cmdtaskmanager/shared/json_file.py
@@ -1,10 +1,10 @@
+import os
 import json
-from .display import display_error
 
 
 def write_json(file_location, json_content):
-    try:
-        with open(file_location, 'w') as file_object:
-            json.dump(json_content, file_object, indent=4)
-    except Exception as _:
-        display_error(f"Can't write to {file_location}")
+    if not os.path.exists(file_location):
+        os.makedirs(os.path.dirname(file_location), exist_ok=True)
+
+    with open(file_location, 'w') as file_object:
+        json.dump(json_content, file_object, indent=4)
