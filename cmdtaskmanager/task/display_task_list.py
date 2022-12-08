61b6b16fcc025a0db33b936b8fb382d02021dfd0
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-08 06:38:36 +0200

    Added display task list functionality

--- cmdtaskmanager/task/display_task_list.py
+++ cmdtaskmanager/task/display_task_list.py
@@ -2,5 +2,5 @@ from .core import get_tasks_to_display
 from .display_core import display_task_list as display_task_list_fn
 
 
-def display_task_list(_):
-    display_task_list_fn(get_tasks_to_display(10))
+def display_task_list(args):
+    display_task_list_fn(get_tasks_to_display(args.limit))
--- cmdtaskmanager/task/display_task_list_subparser.py
+++ cmdtaskmanager/task/display_task_list_subparser.py
@@ -1,9 +1,14 @@
+from ..shared.consts import DEFAULT_DISPLAY_LIMIT
 from .display_task_list import display_task_list as display_task_list_handler
 from ..shared.parser import subparsers
 
 
 def display_task_list_subparser():
     display_task_list = subparsers.add_parser('display-task-list', help='Display task list.')
+    display_task_list.add_argument('-l', '--limit', type=int, required=False,
+                          metavar='',
+                          help="Amount of tasks to display.",
+                          default=DEFAULT_DISPLAY_LIMIT)
     # TODO: Here should be added the query functionality
     # to display task by project, priority, project and priority etc.
     display_task_list.set_defaults(func=display_task_list_handler)
