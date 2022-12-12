f6604cb23e97692a7251f089ddf66dd47fe6e96f
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-12 07:51:36 +0200

    Auto git push ...

--- temp/queries.sql
+++ temp/queries.sql
@@ -5,6 +5,7 @@ select * from task_tag;
 select * from project;
 select * from status_change_reason;
 
+
 select t.id, t.value, tt.task_id, ta.title from task_tag as tt
 join tag as t on t.id = tt.tag_id
 join task as ta on ta.id = tt.task_id
@@ -16,14 +17,6 @@ from tag as t
 join task_tag as tt on tt.tag_id = t.id
 join task as tk on tk.id = tt.task_id
 where t.value = 'RealProject';
-
--- drop all tables
-drop table status_change_reason;
-drop table status;
-drop table task_tag;
-drop table tag;
-drop table project;
-drop table task;
 
 
 -- Find add tasks that have tag with the given id
