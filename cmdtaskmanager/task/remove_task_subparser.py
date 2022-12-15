a6f8eed4f73a270835cd7b826aea9e39702616a6
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-15 17:59:17 +0200

    Fixed remove bug

--- cmdtaskmanager/task/remove_task_subparser.py
+++ cmdtaskmanager/task/remove_task_subparser.py
@@ -3,6 +3,6 @@ from ..shared.parser import subparsers
 
 
 def remove_task_subparser():
-    add_task = subparsers.add_parser('remove-task', help='Remove task.')
+    add_task = subparsers.add_parser('full-remove-task', help='Remove task.')
     add_task.add_argument('task_id', type=int, help='Task id.')
     add_task.set_defaults(func=remove_task_handler)
