6910e257a48e687996ebead8ef4213620e5c3c70
Oleg <oleh.baranovskyi.dev.acc@gmail.com>
2022-12-16 07:24:44 +0200

    Auto git push ...

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@ echo 'Installing cmdtaskmanager...'
 mkdir -p ~/.config
 cd ~/.config
 python3 -m pip install -e git+https://github.com/obaranovskyi/cmdtaskmanager.git#egg=cmdtaskmanager
-cd ~/.config/src/cmdenglishassist
+cd ~/.config/src/cmdtaskmanager
 pip install -r requirements.txt
 echo 'cmdtaskmanager installed'
